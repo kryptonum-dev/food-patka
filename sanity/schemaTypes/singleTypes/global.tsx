b360fa004b5bd7ddcbb97940094a821d424a9f6c
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-04-16 14:23:05 +0200

    added global schema

---
+++ sanity/schemaTypes/singleTypes/global.tsx
@@ -0,0 +1,103 @@
+import { defineField, defineType } from 'sanity'
+
+const OrganizationSchema = [
+  defineField({
+    name: 'name',
+    type: 'string',
+    title: 'Nazwa Twojej organizacji',
+  }),
+  defineField({
+    name: 'description',
+    type: 'text',
+    rows: 3,
+    title: 'Szczegółowy opis Twojej organizacji',
+  }),
+]
+
+export const global = defineType({
+  name: 'global',
+  type: 'document',
+  title: 'Globalne',
+  icon: () => '🌍',
+  fields: [
+    defineField({
+      name: 'email',
+      type: 'string',
+      title: 'Adres e-mail',
+      validation: Rule => Rule.required().email(),
+    }),
+    defineField({
+      name: 'tel',
+      type: 'string',
+      title: 'Numer telefonu',
+    }),
+    defineField({
+      name: 'socials',
+      type: 'object',
+      title: 'Social media',
+      options: { collapsible: true, collapsed: true },
+      fields: [
+        defineField({
+          name: 'facebook',
+          type: 'url',
+          title: 'Facebook',
+          validation: Rule => Rule.required().uri({ scheme: ['https'] }),
+        }),
+        defineField({
+          name: 'instagram',
+          type: 'url',
+          title: 'Instagram',
+          validation: (Rule) => Rule.required().uri({ scheme: ['https'] }),
+        }),
+        defineField({
+          name: 'youtube',
+          type: 'url',
+          title: 'YouTube',
+          validation: (Rule) => Rule.required().uri({ scheme: ['https'] }),
+        }),
+        defineField({
+          name: 'messenger',
+          type: 'url',
+          title: 'Messenger',
+          validation: (Rule) => Rule.required().uri({ scheme: ['https'] }),
+        }),
+      ],
+    }),
+    defineField({
+      name: 'seo',
+      type: 'object',
+      title: 'Globalne SEO',
+      fields: [
+        defineField({
+          name: 'og_Img',
+          type: 'image',
+          title: 'OG Image',
+          description:
+            'Zdjęcie, które jest widoczne przy udostępnianiu strony w mediach społecznościowych. Wymiary zdjęcia powinny mieć 1200x630px',
+        }),
+      ],
+    }),
+    defineField({
+      name: 'OrganizationSchema',
+      type: 'object',
+      title: 'Uporządkowane dane organizacji',
+      description: (
+        <a
+          href="https://developers.google.com/search/docs/appearance/structured-data/organization?hl=pl"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Więcej informacji o Schema
+        </a>
+      ),
+      fields: OrganizationSchema,
+      options: { collapsible: true, collapsed: true },
+    }),
+    // {
+    //   name: 'CookieConsent',
+    //   type: 'CookieConsent',
+    //   title: 'Baner z informacją o ciasteczkach',
+    // },
+  ],
+})
+
