4df52d663dbbe5f6fbcd3ef75891ac113d9993e0
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-04-18 13:44:56 +0200

    added string to global REGEX collection

--- next/src/global/constants.ts
+++ next/src/global/constants.ts
@@ -44,7 +44,8 @@ export const LOGO_URL: string = `${DOMAIN}/food-patka-logo.png`;
  * Global declaration of regex.
  * @constant
  */
-export const REGEX: { email: RegExp; phone: RegExp } = {
+export const REGEX: { email: RegExp; phone: RegExp; string: RegExp } = {
   email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
   phone: /^(?:\+(?:\d{1,3}))?(?:[ -]?\(?\d{1,4}\)?[ -]?\d{1,5}[ -]?\d{1,5}[ -]?\d{1,6})$/,
+  string: /^(?!\s+$)(.*?)\s*$/,
 };
