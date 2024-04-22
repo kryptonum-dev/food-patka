a55575d32c85327e346b76f206937c23dd2d0bc8
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-04-22 17:23:44 +0200

    fixed Footer Wave aspectRatio

--- next/src/components/global/Footer/Footer.tsx
+++ next/src/components/global/Footer/Footer.tsx
@@ -54,6 +54,7 @@ const WaveSvg = ({ ...props }) => (
     xmlns="http://www.w3.org/2000/svg"
     width='100%'
     viewBox='0 0 1366 175'
+    preserveAspectRatio='none'
     fill="none"
     {...props}
   >
