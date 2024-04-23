ca2d33e05395c892ce0aa9bcd38270f938c55a30
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-04-23 14:14:59 +0200

    changed margin for isInView in Counter

--- next/src/components/global/SimpleStats/_Counter.tsx
+++ next/src/components/global/SimpleStats/_Counter.tsx
@@ -7,7 +7,7 @@ export default function Counter({ value }: CounterTypes) {
   const ref = useRef<HTMLSpanElement>(null);
   const motionValue = useMotionValue(0);
   const springValue = useSpring(motionValue, { damping: 100, stiffness: 100 });
-  const isInView = useInView(ref, { once: true, margin: '-100px' });
+  const isInView = useInView(ref, { once: true, margin: '0px' });
 
   useEffect(() => {
     if (isInView) motionValue.set(value);
