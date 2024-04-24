438e1940d93173aac9b5537c61cca7e7612e67ef
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-04-24 16:33:31 +0200

    added ContactPage

---
+++ next/src/app/kontakt/page.tsx
@@ -0,0 +1,36 @@
+import sanityFetch from '@/utils/sanity.fetch';
+import { QueryMetadata } from '@/global/Seo/query-metadata';
+import Breadcrumbs from '@/components/global/Breadcrumbs';
+import Components, { ComponentTypes, Components_Query } from '@/components/Components';
+
+const currentPath = '/kontakt';
+const breadcrumbs = [
+  { name: 'Kontakt', path: currentPath },
+];
+
+export default async function ContactPage() {
+  const { content } = await query();
+
+  return (
+    <>
+      <Breadcrumbs data={breadcrumbs} />
+      <Components data={content} />
+    </>
+  );
+}
+
+const query = async (): Promise<{ content: ComponentTypes[] }> => {
+  return await sanityFetch({
+    query: /* groq */ `
+      *[_type == "Contact_Page"][0] {
+        ${Components_Query}
+      }
+    `,
+    tags: ['Contact_Page'],
+  });
+};
+
+
+export async function generateMetadata() {
+  return await QueryMetadata('Contact_Page', currentPath);
+}
