a64ba407e86d06461d2ce8f0c8889c7ea74146c5
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-05-15 16:26:16 +0200

    updated robots.txtg

--- next/src/app/robots.tsx
+++ next/src/app/robots.tsx
@@ -1,10 +1,12 @@
+import type { MetadataRoute } from 'next';
 import { DOMAIN } from '@/global/constants';
 
-export default function robots() {
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: ['/dziekujemy-za-zakup'],
     },
     sitemap: `${DOMAIN}/sitemap.xml`,
   };
