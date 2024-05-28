2502e748ba0e74b2c5d0aee54b79f5e61f913b8b
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-05-28 16:18:10 +0200

    updated BlogPostCard for accessibility and performance

--- next/src/components/global/BlogPostCard/BlogPostCard.tsx
+++ next/src/components/global/BlogPostCard/BlogPostCard.tsx
@@ -9,13 +9,26 @@ import type { BlogPostCardTypes } from './BlogPostCard.types';
 export default function BlogPostCard({ title, subtitle, img, slug }: BlogPostCardTypes) {
   return (
     <article className={styles['BlogPostCard']}>
-      <Link href={`/blog/${slug}`} className={styles.img}>
-        <Img data={img} sizes='' />
-      </Link>
+      <Link
+        href={`/blog/${slug}`}
+        aria-label={`Przeczytaj artykuł: ${removeMarkdown(title)}`}
+        className={styles.link}
+      />
+      <div className={styles.img}>
+        <Img
+          data={img}
+          sizes='(max-width: 499px) 448px, (max-width: 1049px) 490px, 380px'
+        />
+      </div>
       <div className={styles.wrapper}>
         <Markdown.h2 className='h3'>{removeMarkdown(title)}</Markdown.h2>
         <Markdown className={styles.subtitle}>{subtitle}</Markdown>
-        <Button href={`/blog/${slug}`}>Przeczytaj więcej</Button>
+        <Button
+          href={`/blog/${slug}`}
+          className={styles.cta}
+        >
+          Przeczytaj więcej
+        </Button>
       </div>
     </article>
   );
