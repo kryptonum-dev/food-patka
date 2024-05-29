5588a414271e027377a018b849697d79174b5c55
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-05-29 17:10:50 +0200

    Update HighlightedSection component with media fieldset and video option

--- sanity/schema/collectionTypes/Product_Collection/HighlightedSection.ts
+++ sanity/schema/collectionTypes/Product_Collection/HighlightedSection.ts
@@ -26,7 +26,7 @@ export default defineField({
       name: 'isReversed',
       type: 'boolean',
       title: 'Czy odwrócić kolejność?',
-      description: 'Domyślnie zdjęcie jest z lewej strony. Jeśli odwrócisz kolejność to zdjęcie będzie po prawej.',
+      description: 'Domyślnie zdjęcie jest z lewej strony. Jeśli odwrócisz kolejność to zdjęcie będzie po prawej. Tylko jedno uzupełnione pole jest możliwe do uzupełnienia.',
       initialValue: false,
       hidden: ({ parent }) => !parent?.img,
     }),
@@ -34,7 +34,27 @@ export default defineField({
       name: 'img',
       type: 'image',
       title: 'Zdjęcie (opcjonalne)',
+      fieldset: 'media',
+      hidden: ({ parent }) => !!parent?.video,
     }),
+    defineField({
+      name: 'video',
+      type: 'file',
+      options: {
+        accept: 'video/*',
+      },
+      title: 'Wideo (opcjonalne)',
+      fieldset: 'media',
+      hidden: ({ parent }) => !!parent?.img,
+    }),
+  ],
+  fieldsets: [
+    {
+      name: 'media',
+      title: 'Zdjęcie lub film',
+      description: 'Możesz dodać zdjęcie lub film.',
+      options: { columns: 2 },
+    }
   ],
   preview: {
     select: {
