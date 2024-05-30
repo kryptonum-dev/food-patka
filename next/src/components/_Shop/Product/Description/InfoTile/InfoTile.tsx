3ac1882a3e65f30c7eef60dbbdf15aa64ec4c47e
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-05-30 14:10:28 +0200

    Update image sizes in InfoTile module for responsive design

--- next/src/components/_Shop/Product/Description/InfoTile/InfoTile.tsx
+++ next/src/components/_Shop/Product/Description/InfoTile/InfoTile.tsx
@@ -6,7 +6,7 @@ import type { InfoTileTypes } from './InfoTile.types';
 export default function InfoTile({ img, heading, paragraph }: InfoTileTypes) {
   return (
     <section className={styles['InfoTile']}>
-      <Img data={img} sizes='174px' />
+      <Img data={img} sizes='(max-width: 449px) 100vw, 188px' />
       <header>
         <Markdown.h2 className='h3'>{heading}</Markdown.h2>
         <Markdown className={styles.paragraph}>{paragraph}</Markdown>
