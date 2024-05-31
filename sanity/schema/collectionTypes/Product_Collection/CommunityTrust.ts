d837785847f379667e250b5581c2adf23091715c
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-05-31 09:39:29 +0200

    feat: Add initial images to CommunityTrust field in Product_Collection schema

--- sanity/schema/collectionTypes/Product_Collection/CommunityTrust.ts
+++ sanity/schema/collectionTypes/Product_Collection/CommunityTrust.ts
@@ -18,6 +18,12 @@ export default defineField({
       ],
       title: 'Zdjęcia',
       description: 'Zdjęcia osób, które zaufały marce. Minumum 2 zdjęcia, maksimum 4 zdjęcia.',
+      initialValue: [
+        { _type: 'image', asset: { _type: 'reference', _ref: 'image-a3262b2762d9bd3479d8424842514f688fb4f23c-96x96-webp' } },
+        { _type: 'image', asset: { _type: 'reference', _ref: 'image-8d5f9ec4a0f62db889609461bee4e9f4baf291be-96x96-webp' } },
+        { _type: 'image', asset: { _type: 'reference', _ref: 'image-16cfa59ee8024213a3d35a4737cba5bac71841f4-96x96-webp' } },
+        { _type: 'image', asset: { _type: 'reference', _ref: 'image-3487268b9c7f6da5f82275a4e026fda0a2b59180-96x96-webp' } },
+      ],
       validation: Rule => Rule.required().min(2).max(4),
     }),
     defineField({
