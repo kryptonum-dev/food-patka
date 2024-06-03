77354c39eae324d10a34b0aa45e186a3fcaecb00
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-06-03 17:55:13 +0200

    Add GoogleTagManager to layout

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/global/global.scss';
 import { LOCALE, THEME_COLOR } from '@/global/constants';
 import { LibreBaskerville, Rosario } from '@/global/fonts';
+import { GoogleTagManager } from '@next/third-parties/google';
 import Header from '@/components/global/Header';
 import Footer from '@/components/global/Footer';
 import CookieConsent from '@/components/global/CookieConsent';
@@ -18,6 +19,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Footer />
         <CookieConsent />
       </body>
+      {/* {process.env.NODE_ENV === 'production' && <GoogleTagManager gtmId='GTM-KNPVL7L7' />} */}
+      <GoogleTagManager gtmId='GTM-KNPVL7L7' />
     </html>
   );
 }
