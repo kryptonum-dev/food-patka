899f878520ff2ae275377cb233a743ea28a38c83
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-06-04 14:17:35 +0200

    update getCookie util

--- next/src/utils/get-cookie.ts
+++ next/src/utils/get-cookie.ts
@@ -2,8 +2,9 @@ export const getCookie = (name: string): string | null => {
   const cookieString = document.cookie;
   const cookies = cookieString.split(';').map((cookie) => cookie.trim());
   for (const cookie of cookies) {
-    const [cookieName, cookieValue] = cookie.split('=').map((part) => part.trim()) as [string, string];
+    const [cookieName, ...cookieParts] = cookie.split('=');
     if (cookieName === name) {
+      const cookieValue = cookieParts.join('=');
       return decodeURIComponent(cookieValue);
     }
   }
