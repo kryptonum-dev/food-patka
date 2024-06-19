894aa3056c9fa8d691b28475bd69e6384b13789f
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-06-19 15:29:13 +0200

    Add InstagramShowcase schema

--- sanity/schema/components/InstagramShowcase.ts
+++ sanity/schema/components/InstagramShowcase.ts
@@ -16,15 +16,54 @@ export default defineField({
       title: 'Nagłówek',
       validation: Rule => Rule.required(),
     }),
+    defineField({
+      name: 'list',
+      type: 'array',
+      of: [
+        defineField({
+          name: 'post',
+          type: 'object',
+          title: 'Post',
+          fields: [
+            defineField({
+              name: 'url',
+              type: 'url',
+              title: 'Link do posta',
+              validation: Rule => Rule.required().uri({ scheme: ['https'] }),
+            }),
+            defineField({
+              name: 'img',
+              type: 'image',
+              title: 'Zdjęcie',
+              validation: Rule => Rule.required(),
+            }),
+          ],
+          preview: {
+            select: {
+              url: 'url',
+              img: 'img',
+            },
+            prepare: ({ url, img }) => ({
+              title: url,
+              media: img,
+            }),
+          }
+        })
+      ],
+      title: 'Lista zdjęć',
+      validation: Rule => Rule.required().max(3),
+    }),
   ],
   preview: {
     select: {
       heading: 'heading',
+      list: 'list',
     },
-    prepare: ({ heading }) => ({
+    prepare: ({ heading, list }) => ({
       title: title,
       subtitle: removeMarkdown(heading),
       icon,
+      media: list.length > 0 && list[0]?.img,
     }),
   },
 });
