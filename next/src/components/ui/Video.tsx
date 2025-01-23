f546db8a1a678c3b7180d18e6e4afce25d37e50b
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2025-01-23 11:47:54 +0100

    remove blurp from mux

--- next/src/components/ui/Video.tsx
+++ next/src/components/ui/Video.tsx
@@ -1,5 +1,4 @@
 import MuxPlayer from '@mux/mux-player-react/lazy';
-import { createBlurUp } from '@mux/blurup';
 
 type Props = {
   playbackId: string
@@ -16,14 +15,8 @@ export const VideoDataQuery = (name: string) => `
   },
 `;
 
-const getPlaceholder = async (playbackId: string) => {
-  const { blurDataURL } = await createBlurUp(playbackId);
-  return blurDataURL;
-};
-
-export default async function Video({ playbackId, aspectRatio, hideControls, ...props }: Props) {
+export default function Video({ playbackId, aspectRatio, hideControls, ...props }: Props) {
   const aspect_ratio = aspectRatio.replace(':', ' / ');
-  const placeholder = await getPlaceholder(playbackId);
 
   return (
     <MuxPlayer
@@ -33,7 +26,7 @@ export default async function Video({ playbackId, aspectRatio, hideControls, ...
       disableTracking
       primaryColor="#fffdfd"
       accentColor="#f489a9"
-      placeholder={placeholder}
+      placeholder="data:image/svg+xml;charset=utf-8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; width=&quot;100%&quot; height=&quot;100%&quot;><filter id=&quot;b&quot; color-interpolation-filters=&quot;sRGB&quot;><feGaussianBlur stdDeviation=&quot;20&quot;/><feComponentTransfer><feFuncA type=&quot;discrete&quot; tableValues=&quot;1 1&quot;/></feComponentTransfer></filter><g filter=&quot;url(%23b)&quot;><image width=&quot;100%&quot; height=&quot;100%&quot; preserveAspectRatio=&quot;xMidYMid slice&quot; href=&quot;data:image/webp;base64,UklGRp4AAABXRUJQVlA4IJIAAABwAgCdASoJABAAAQAcJQBOhgqpEQl/xLKPOAVUAP0DIHz9POk6vFxxMoYdWIYvSoifPajB+tbpDKWmUNsS03iRFBs6Xs/RFs1sZfch6nlSlbEE2kVd+4A52mrbOZTeMbIY42od3creJ8vOdFCqCWHSKXHG5UQ79P6fT7xMX/NwUcnnT7DdAWswYhsOfz1Q4XgAAA==&quot;/></g></svg>"
       style={{
         aspectRatio: aspect_ratio,
         display: 'block',
